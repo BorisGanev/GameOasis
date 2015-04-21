168729d32d39efa37e38ff6001a8b724acf8f8b1
DingoTheMagicDragon <boris.ganev@hotmail.com>
2015-04-21 11:33:44 +0200

    Initial commit

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GameCatalog()
+        {
+          return View();
+        }
+
+    }
+
+    public class GameDirectoryController : Controller
+    {
+        public ActionResult Dota2()
+        {
+            return View();
+        }
+    }
+
+    public class NewsDirectoryController : Controller
+    {
+        public ActionResult BloodborneNews()
+        {
+            return View();
+        }
+
+    }
+
+
+}
